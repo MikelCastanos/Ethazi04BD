074bfe163729ec94ebffa0a626a5a74f92235386
Iraide <arrizabalagairaide@gmail.com>
2019-04-05 21:12:48 +0200

    Consulta realizada desde otra clase

--- src/BBDD/Conexion.java
+++ src/BBDD/Conexion.java
@@ -16,20 +16,13 @@ import java.sql.ResultSet;
  */
 public class Conexion {
     Connection conectar = null;
-    public Connection conexion(){
+    public Connection conectar(){
    
         try{
             Class.forName("com.mysql.jdbc.Driver");
             conectar=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/ethazi4","root","");
             System.out.println("Conexion establecida");
-//            Crear consulta
-            Statement s = (Statement) conectar.createStatement(); 
-            ResultSet rs = s.executeQuery ("select * from hotel");
-//            Recorrer toda la información a mostrar
-            while (rs.next()) 
-                { 
-                    System.out.println (rs.getInt (1) + " " + rs.getString (2)+ " " + rs.getInt(3)+ " " + rs.getString (4)); 
-                }
+
             }
         
         catch(Exception e){
--- src/BBDD/Consultas.java
+++ src/BBDD/Consultas.java
@@ -5,10 +5,46 @@
  */
 package BBDD;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author ikasleaETHAZI
  */
 public class Consultas {
     
+//    Metodo consulta recibe connection= con y la consulta=query
+    public ResultSet consultaBD(Connection con, String query){
+        ResultSet rs=null;
+        try{
+            
+            Statement st= con.createStatement();
+            rs= st.executeQuery(query);
+            
+        }catch (Exception e){
+           System.out.println(e.getMessage());
+        }
+        return rs;
+    }
+    
+    
+    public boolean insertarDatosBD(Connection con, String query){
+       Statement st;
+       try{
+           st=con.createStatement();
+       }catch(SQLException e){
+           System.out.println(e.getMessage());
+           return false;
+       }
+       try{
+           st.executeUpdate(query);
+       }catch(Exception e){
+           System.out.println(e.getMessage());
+           return false;
+       }
+       return true;
+    }
 }
